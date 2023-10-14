edd079be09d397bed89b56d1bad9f331781714ea
Thi Vu <thi.vu@tuni.fi>
2023-10-14 18:59:45 +0300

    stl2

--- wk07/stl/stl2.cpp
+++ wk07/stl/stl2.cpp
@@ -7,6 +7,10 @@
 using namespace std;
 
 
+bool descending(int a, int b) {
+    return a > b;
+}
+
 /**
  * @brief sort a given vector to an descending order
  *
@@ -15,6 +19,7 @@ using namespace std;
  */
 int sortDesc(std::vector<int>& v)
 {
-    return EXIT_FAILURE;
+    std::sort(v.begin(), v.end(), descending);
+    return EXIT_SUCCESS;
 }
 

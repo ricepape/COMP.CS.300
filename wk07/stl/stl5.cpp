218d9932f42a316f44eefe00e82e7717eeff4855
Thi Vu <thi.vu@tuni.fi>
2023-10-15 16:49:24 +0300

    stl5

--- wk07/stl/stl5.cpp
+++ wk07/stl/stl5.cpp
@@ -17,6 +17,21 @@ using namespace std;
  */
 int sortMod3(std::vector<int>& v)
 {
-    return EXIT_FAILURE;
+    std::vector<int> mod0, mod1, mod2;
+
+    std::copy_if(v.begin(), v.end(), std::back_inserter(mod0), [](int num) { return num % 3 == 0; });
+    std::copy_if(v.begin(), v.end(), std::back_inserter(mod1), [](int num) { return num % 3 == 1; });
+    std::copy_if(v.begin(), v.end(), std::back_inserter(mod2), [](int num) { return num % 3 == 2; });
+
+    std::sort(mod0.begin(), mod0.end());
+    std::sort(mod1.begin(), mod1.end());
+    std::sort(mod2.begin(), mod2.end());
+
+    v.clear();
+    v.insert(v.end(), mod0.begin(), mod0.end());
+    v.insert(v.end(), mod1.begin(), mod1.end());
+    v.insert(v.end(), mod2.begin(), mod2.end());
+
+    return EXIT_SUCCESS;
 }
 

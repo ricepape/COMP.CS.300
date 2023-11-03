f923c3845e00b1b81a3e1182fd878f19527bbe92
Thi Vu <thi.vu@tuni.fi>
2023-11-03 22:39:54 +0200

    improve11

--- wk09/improving_functions/improve1.cc
+++ wk09/improving_functions/improve1.cc
@@ -12,7 +12,7 @@
 std::vector<int> ascendingVector(int n){
     std::vector<int> v;
     for ( int i = 0; i < n ; i++){
-        v.insert(v.end(), i);
+        v.push_back(i);
     }
     return v;
 }
